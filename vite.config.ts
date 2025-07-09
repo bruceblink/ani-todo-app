dabd0e8981e979d4d7f2b6da18ac75c7dec16846
likanug <likanug.g@qq.com>
2025-07-09 13:41:52 +0800

    更新vite的前端配置，与开发的后端环境一直

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3000,    // <-- 改成 1420
+  },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
   plugins: [react()],
 })
