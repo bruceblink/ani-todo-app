7e0486bb201672959f26e6fc23adf4059939f171
likanug <likanug.g@qq.com>
2025-07-12 15:06:26 +0800

    更新页面的布局样式

--- src/App.tsx
+++ src/App.tsx
@@ -529,32 +529,27 @@ const ani_data = {
 }
 
 export default function App() {
-    const ani_list = ani_data["星期五"].map((ani) => {
-        return (
-
-            <div className="ani-item" key={ani.title}>
-                <a href={ani.detail_url} target="_blank" rel="noopener noreferrer">
-                    <img src={ani.image_url}
-                         alt={ani.title}
-                         style={{
-                             width: 150,
-                             height: 150
-                         }}
-                    />
-                    <div className="ani-title">{ani.title}</div>
-                    <div className="ani-update-info">{ani.update_info}</div>
-                    <div className="ani-platform">{ani.platform}</div>
-                </a>
+    const ani_list = ani_data["星期五"].map((ani, idx) => (
+        <div className="ani-item" key={`${ani.detail_url}-${idx}`}>
+            <a href={ani.detail_url} target="_blank" rel="noopener noreferrer">
+                <img className="ani-img" src={ani.image_url} alt={ani.title} />
+            </a>
+            <div className="ani-info">
+                <div className="ani-title">{ani.title}</div>
+                <div className="ani-update-info">{ani.update_info}</div>
+                <div className="ani-platform">{ani.platform}</div>
             </div>
-        );
-    });
-    const today = Object.keys(ani_data)[0]
+        </div>
+    ));
+
+    const today = Object.keys(ani_data)[0];
+
     return (
         <div className="App">
-            <h1>{today}更新番剧</h1>
-            <div className="ani-list">
-                {ani_list}
-            </div>
+            <h1>{today} 更新番剧</h1>
+            <div className="ani-list">{ani_list}</div>
         </div>
     );
 }
+
+
