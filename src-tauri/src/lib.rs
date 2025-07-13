653d5124e1c8fff9aabd444eacfce38ff8334c74
likanug <likanug.g@qq.com>
2025-07-13 16:23:03 +0800

    feat: rust代码lib.rs中新增通用AniItem结构体

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,2 +1,16 @@
+use serde::{Deserialize, Serialize};
+
 pub mod startup;
+pub mod utils;
 pub mod platforms;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AniItem {
+    pub title: String,
+    pub update_count: String,
+    pub update_info: String,
+    pub image_url: String,
+    pub detail_url: String,
+    pub update_time: String,
+    pub platform: String,
+}
