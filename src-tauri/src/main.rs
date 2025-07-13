ca67d06b098ed1073a8d9616d10c344c8ed81427
likanug <likanug.g@qq.com>
2025-07-13 17:45:51 +0800

    refactor: 使用默认的crate名称

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use app_lib::startup::run;
+use ani_todo_app::startup::run;
 
 fn main() {
     run()
