775134366183147c7443899ad6ba4b0bb974a4ad
likanug <likanug.g@qq.com>
2025-07-13 18:22:55 +0800

    fix: 删除startup.rs移动代码到lib.rs中修复无法找打tauri:command的问题

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,22 @@
-pub mod startup;
 pub mod utils;
 pub mod platforms;
 
-pub use platforms::fetch_bilibili_ani_data;
+use crate::platforms::{fetch_bilibili_ani_data, fetch_bilibili_image};
 
-pub use platforms::fetch_bilibili_image;
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![fetch_bilibili_image, fetch_bilibili_ani_data])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use ani_todo_app::startup::run;
+use app_lib::run;
 
 fn main() {
     run()
