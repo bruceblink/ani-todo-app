43c1f2525208aca59e67564ddaa3a0bafc70ba03
likanug <likanug.g@qq.com>
2025-07-13 20:56:53 +0800

    feat: 设置日志输出的的时区为本地时区

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,15 +2,30 @@ pub mod utils;
 pub mod platforms;
 
 use crate::platforms::{fetch_bilibili_ani_data, fetch_bilibili_image};
+use chrono::Local; 
+use std::fmt;
+use tauri_plugin_log::fern;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(debug_assertions) {
+                // 自定义日志格式（使用本地时区）
+                let format = move |out: fern::FormatCallback, message: &fmt::Arguments, record: &log::Record| {
+                    let now = Local::now();  // 获取本地时间
+                    out.finish(format_args!(
+                        "[{}][{}][{}] {}",
+                        now.format("%Y-%m-%d %H:%M:%S%.3f"),  // 格式化为本地时间
+                        record.level(),
+                        record.target(),
+                        message
+                    ))
+                };
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
+                        .format(format)  // 应用自定义格式
                         .build(),
                 )?;
             }
