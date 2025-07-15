b5b0c61dbd8ed1166f5c959d28c9b4124d7b2bd2
likanug <likanug.g@qq.com>
2025-07-15 11:45:50 +0800

    test: 增加对获取蜜柑计划的测试用例

--- src-tauri/tests/fetch_test.rs
+++ src-tauri/tests/fetch_test.rs
@@ -1,5 +1,6 @@
 use app_lib::platforms::bilibili::fetch_bilibili_ani_data;
 use app_lib::platforms::iqiyi::fetch_iqiyi_ani_data;
+use app_lib::platforms::mikanani::fetch_mikanani_ani_data;
 
 #[tokio::test]
 async fn test_fetch_bilibili_ani_data() {
@@ -12,5 +13,12 @@ async fn test_fetch_bilibili_ani_data() {
 async fn test_iqiyi_ani_data() {
     let url = "https://mesh.if.iqiyi.com/portal/lw/v7/channel/cartoon".to_string();
     let res = fetch_iqiyi_ani_data(url).await.unwrap();
+    println!("{:?}", res.to_string());
+}
+
+#[tokio::test]
+async fn test_mikanani_ani_data() {
+    let url = "https://mikanani.me".to_string();
+    let res = fetch_mikanani_ani_data(url).await.unwrap();
     println!("{:?}", res.to_string());
 }
