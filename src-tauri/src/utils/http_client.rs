086b6fcb983a7f69953e6d79f5109716b5c17728
likanug <likanug.g@qq.com>
2025-07-19 16:39:19 +0800

    feat: 更新http_client()工具方法

--- src-tauri/src/utils/http_client.rs
+++ src-tauri/src/utils/http_client.rs
@@ -8,24 +8,23 @@ pub fn http_client() -> Result<Client, String> {
     headers.insert("Accept", HeaderValue::from_static(
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
     ));
-    headers.insert("Accept-Encoding", HeaderValue::from_static("gzip, deflate, br, zstd"));
+    // headers.insert("Accept-Encoding", HeaderValue::from_static("gzip, deflate, br, zstd"));
     headers.insert("Accept-Language", HeaderValue::from_static("zh-CN,zh;q=0.9,en;q=0.8"));
     headers.insert("Cache-Control", HeaderValue::from_static("no-cache"));
     headers.insert("Cookie", HeaderValue::from_static("cleanMode=0"));
     headers.insert("DNT", HeaderValue::from_static("1"));
     headers.insert("Pragma", HeaderValue::from_static("no-cache"));
+    headers.insert("priority", HeaderValue::from_static("u=0, i"));
     headers.insert("Sec-Ch-Ua", HeaderValue::from_static(
-        "\"Google Chrome\";v=\"137\", \"Chromium\";v=\"137\", \"Not/A)Brand\";v=\"24\""
-    ));
-    headers.insert("Sec-Ch-Ua-Mobile", HeaderValue::from_static("?0"));
-    headers.insert("Sec-Ch-Ua-Platform", HeaderValue::from_static("\"Windows\""));
+        "Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\""));
+    headers.insert("sec-ch-ua-platform", HeaderValue::from_static("\"Windows\""));
     headers.insert("Sec-Fetch-Dest", HeaderValue::from_static("document"));
     headers.insert("Sec-Fetch-Mode", HeaderValue::from_static("navigate"));
     headers.insert("Sec-Fetch-Site", HeaderValue::from_static("none"));
     headers.insert("Sec-Fetch-User", HeaderValue::from_static("?1"));
     headers.insert("Upgrade-Insecure-Requests", HeaderValue::from_static("1"));
     headers.insert("User-Agent", HeaderValue::from_static(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
     ));
 
     Client::builder()
