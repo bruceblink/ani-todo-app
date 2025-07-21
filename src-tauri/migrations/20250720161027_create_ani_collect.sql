eab08053506662c2f91bb91c88886cf3bd6449e5
likanug <likanug.g@qq.com>
2025-07-21 14:36:03 +0800

    feat: 更新ani_collect表，添加唯一约束

--- src-tauri/migrations/20250720161027_create_ani_collect.sql
+++ src-tauri/migrations/20250720161027_create_ani_collect.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS ani_collect (
      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,  -- 主键id
      ani_item_id  INTEGER NOT NULL,                  -- 番剧ID
      collect_time TEXT NOT NULL,                     -- 收藏时间
-     watched INTEGER NOT NULL DEFAULT 0              -- 是否已观看，0为false，1为true
+     watched INTEGER NOT NULL DEFAULT 0,             -- 是否已观看，0为false，1为true
+     UNIQUE(ani_item_id)                             -- 唯一约束
 );
