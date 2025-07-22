dfc6287b1d9297aae16a5ce8596b78d3c612ff19
likanug <likanug.g@qq.com>
2025-07-22 21:01:57 +0800

    chore: 删除无用工具函数

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,21 +1,4 @@
-import {invoke} from '@tauri-apps/api/core';
 import type {Ani} from "@/utils/api";
-/**
- *  从各个视频网站获取最新的动漫更新数据
- * @param url
- * @param cmd
- */
-export async function fetchAniData(url: string, cmd: string) {
-    try {
-        // 调用命令，拿到 JSON 字符串
-        const data = await invoke(cmd, { url }) as Record<string, Ani[]>;
-        console.log(`fetch data from ${url}：`, data);
-        return data;
-    } catch (e) {
-        console.error(`fetch data from ${url} 失败：`, e);
-        return {};
-    }
-}
 
 /**
  * 判断两个标题是否相似
