ed00b21135bd876b930f3f05cc4fd123bfcd1a39
likanug <likanug.g@qq.com>
2025-08-11 16:13:03 +0800

    chore: 更新AboutPage的内容

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const AboutPage = () => {
     return (
         <div style={{ padding: '24px', textAlign: 'center' }}>
             <h2>关于本应用</h2>
-            <p>这是一个用于追踪新番的应用，基于 React 和 Tailwind CSS 构建。</p>
+            <p>这是一个用于追踪新番的应用，基于 React、Tailwind CSS、 Vite + Tauri 构建。</p>
             <Link to="/">返回主页</Link>
         </div>
     );
