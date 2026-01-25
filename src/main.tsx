1aab97adccda4fc63a69d29137e86b7a29296f0e
likanug <likanug.g@qq.com>
2026-01-25 21:48:57 +0800

    feat: disable right click menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { AppProvider } from '@/contexts/AppProvider.tsx';
+import {AppProvider} from '@/contexts/AppProvider.tsx';
+
+// prevent right click menu
+document.addEventListener('contextmenu', (e) => e.preventDefault());
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
